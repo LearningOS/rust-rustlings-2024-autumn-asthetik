94d5eb4491405f7cd3f57ad012f8630da6d8c1de
asthetik <aestheticslee@outlook.com>
2024-10-17 21:25:20 +0800

    update algorithm2

--- exercises/algorithm/algorithm2.rs
+++ exercises/algorithm/algorithm2.rs
@@ -2,7 +2,6 @@
 	double linked list reverse
 	This problem requires you to reverse a doubly linked list
 */
-// I AM NOT DONE
 
 use std::fmt::{self, Display, Formatter};
 use std::ptr::NonNull;
@@ -31,13 +30,13 @@ struct LinkedList<T> {
     end: Option<NonNull<Node<T>>>,
 }
 
-impl<T> Default for LinkedList<T> {
+impl<T: Clone> Default for LinkedList<T> {
     fn default() -> Self {
         Self::new()
     }
 }
 
-impl<T> LinkedList<T> {
+impl<T: Clone> LinkedList<T> {
     pub fn new() -> Self {
         Self {
             length: 0,
@@ -74,6 +73,14 @@ impl<T> LinkedList<T> {
     }
 	pub fn reverse(&mut self){
 		// TODO
+        let mut list = LinkedList::new();
+        let mut node = &self.end;
+        for _ in 0..self.length {
+            let val = unsafe { &(*node.unwrap().as_ptr()).val };
+            list.add(val.clone());
+            node = unsafe { &(*node.unwrap().as_ptr()).prev };
+        }
+        *self = list;
 	}
 }
 
